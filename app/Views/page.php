abe94e2de8da351566f7e3e4374107f6439b4e67
melanierefman <87106838+melanierefman@users.noreply.github.com>
2023-11-13 09:25:45 +0700

    add helloworld code igniter

---
+++ app/Views/page.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat Datang di Website Ini</title>
+    <!-- Menggunakan Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row" style="height: 100vh; justify-content: center; align-items: center;">
+            <div class="col-md-8 text-center">
+                <h1 style="font-weight: bold; color: #007BFF;">Selamat Datang di Website Ini</h1>
+                <p style="color: #333;">Website ini adalah tempat yang hebat untuk mengeksplorasi dan belajar lebih banyak.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Menggunakan Bootstrap JS (Optional) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
